d1060710438ef369b5fc4f7ad7052a6dd04c4ac9
Aziz Rakhmanov <zzrxmnv1002@gmail.com>
2023-07-27 09:58:40 +0500

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Space
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool determiner = true;
+            Console.WriteLine("Enter the sides of triangle");
+
+            Console.Write("a = ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            float b = float.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            float c = float.Parse(Console.ReadLine());
+
+            determiner = (a < (b + c)) && (b < (a + c)) && (c < (a + b));
+
+            if (determiner)
+            {
+                float p = (a + b + c) / 2;
+                double s = p * (p - a) * (p - c) * (p - b);
+                s = Math.Sqrt(s);
+                Console.WriteLine($"Triangle square is {s}");
+            }
+            else
+            {
+                Console.WriteLine("Sides don't make the triangle");
+            }
+
+            Console.WriteLine("Choose 1 to continue or anything to quit");
+
+            string option = Console.ReadLine();
+
+            if (option == "1")
+                Main(args);
+            else
+                return;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Geometre-Guru
-This program is used as calculator of triangle
+### This program is used as calculator of triangle
+
+![Alt text](istockphoto-1012035252-612x612.jpg)
+
